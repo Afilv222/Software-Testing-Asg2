e0366e1bf3f9b383d565b494435b247f46e9845c
Sergei Alejandro <55064632+SergeiAlejandro@users.noreply.github.com>
2025-02-03 15:57:09 -0700

    Update EqualsRangeTest.java

--- test/org/jfree/data/EqualsRangeTest.java
+++ test/org/jfree/data/EqualsRangeTest.java
@@ -11,4 +11,39 @@ class EqualsRangeTest {
 		fail("Not yet implemented");
 	}
 
+	// ++++++++++++++++++++++ EQUALS TEST ++++++++++++++++++++++
+
+	// when both ranges are the same return true
+	@Test
+	void equals_WhenRangesSameReturnTrue() {
+		Range range1 = new Range(1.0, 5.0);
+		Range range2 = new Range(1.0, 5.0);
+
+		boolean actualRange = range1.equals(range2);
+
+		Assert.assertTrue(actualRange);
+	}
+	
+	// when both ranges are the different return false
+	@Test
+	void equals_WhenRangesDiffFalse() {
+		Range range1 = new Range(1.0, 5.0);
+		Range range2 = new Range(2.0, 6.0);
+
+		boolean actualRange = range1.equals(range2);
+
+		Assert.assertFalse(actualRange);
+	}
+
+	// when comparing with null value return false
+	@Test
+	void equals_WhenComparingNullFalse() {
+		Range range1 = new Range(1.0, 5.0);
+		Range range2 = null;
+
+		boolean actualRange = range1.equals(range2);
+
+		Assert.assertFalse(actualRange);
+	}
+
 }
