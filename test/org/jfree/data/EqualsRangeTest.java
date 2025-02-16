1f0ce50dc5f419f9bc76de6a30baecd9aef79a53
SchweitzCodes <mjschweitz17@gmail.com>
2025-02-15 20:56:40 -0700

    Sergei dumb dumb

--- test/org/jfree/data/EqualsRangeTest.java
+++ test/org/jfree/data/EqualsRangeTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 class EqualsRangeTest {
 
